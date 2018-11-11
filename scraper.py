7c442aba241a5a358c690762008a933f4c32c30d
starbucksdolcelatte <seoyoon9535@gmail.com>
2018-11-12 06:11:12 +0900

    Solve Korean encoding problem

--- scraper.py
+++ scraper.py
@@ -1,23 +1,23 @@
-
-'''
-me02_view_cont02 > table > tbody
-    'me02_view_cont01 > p'
-'''
-# parser.py
 import requests
 from bs4 import BeautifulSoup
-import json
-import os
+BASE_URL = 'http://kkc.or.kr/megazine/megazine_02_view.html?idx=3&page=1&key='
+response = requests.get(BASE_URL)
 
+print(response.status_code)
+dom = BeautifulSoup(response.content, "html.parser")
+post_elements = dom.select('div.kind')
+
+print(post_elements)
+
+'''
 # python파일의 위치
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-req = requests.get('http://kkc.or.kr/megazine/megazine_02_view.html?idx=3&page=1&key=')
+req = requests.get()
 html = req.text
 soup = BeautifulSoup(html, 'html.parser')
 my_titles = soup.select(
-
-    'body > div.kind'
+    'div.kind'
     )
 
 data = {}
@@ -27,3 +27,10 @@ for title in my_titles:
 
 with open(os.path.join(BASE_DIR, 'result.json'), 'w+') as json_file:
     json.dump(data, json_file)
+a = json.dumps(data)
+print(data)
+print(type(data))
+print(a)
+print(type(a))
+print(chardet.detect(a))
+'''
